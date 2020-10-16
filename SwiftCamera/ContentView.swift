a97354bd0c508a821c1411aabe6423ae0fabe82b
Rolando Rodríguez <cxavier71@gmail.com>
2020-10-15 22:21:45 -0500

    Initial Commit

---
+++ SwiftCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCamera
+//
+//  Created by Rolando Rodriguez on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCamera/SwiftCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCameraApp.swift
+//  SwiftCamera
+//
+//  Created by Rolando Rodriguez on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
